36382fcbbd7491e5a59b060ab51aeabb542ba7ba
John Varghese 2 <john_varghese2@intuit.com>
2019-06-11 00:04:59 -0700

    welcome to acd

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello AWS Community</h1>
+    <p>Welcome to the 2019 ACD Bay Area.</p>
+    <p>Let's build something great!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
