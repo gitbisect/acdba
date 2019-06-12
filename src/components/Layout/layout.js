776c97ea58c011b4f1281d2e79cfee8112447373
John Varghese 2 <john_varghese2@intuit.com>
2019-06-12 09:48:37 -0700

    moved layout, seo, and header to their own component directories

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./Header/header"
+import Header from "../Header/header"
 import "./layout.css"
 console.log('The layout has been loaded');
 
